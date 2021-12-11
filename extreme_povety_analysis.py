aa58f164e811088745c3032b3e11d4d6c8591521
Amon <77292585+AmonBrollo@users.noreply.github.com>
2021-12-10 21:35:56 -0300

    Update extreme_povety_analysis.py

--- extreme_povety_analysis.py
+++ extreme_povety_analysis.py
@@ -2,19 +2,22 @@ import pandas as pd
 
 def main():
     # Ask a year from the user to investigate
-    year_of_choice = int(input("Which year would you like to invertigate? "))
+    year_of_choice = int(input("Which year would you like to invertigate?(1981-2019) "))
 
+    # Turn the dataset file into a readable dataset
     dataset = pd.read_csv("extreme-poverty-dataset.csv")
 
-    # Find the rows with the chosen year
+    # Get the rows with the chosen year
     rows = dataset.loc[dataset["Year"] == year_of_choice,  "$30.00 per day - share of population below poverty line"]
 
-    countries_largest_bpl = find_largest_portion_by_year(dataset, year_of_choice, rows)
+    # Get the countries with the largest population below the extreme poverty line.
+    countries_largest_bpl = get_largest_year_below(dataset, year_of_choice, rows)
 
-    countries_smallest_bpl = find_smallest_portion_by_year(dataset, year_of_choice, rows)
+    # Get the countries with the smallest population below the extreme poverty line.
+    countries_smallest_bpl = get_smallest_year_below(dataset, year_of_choice, rows)
 
-    # Display the countries with largest portions of people below poverty line and
-    # smallest portions of people below poverty line.
+    # Display the countries with largest portions of people below poverty line and smallest portions of people 
+    # below poverty line.
     print("The countries with the largest portions of people below the poverty line are:")
     for country in countries_largest_bpl:
         print(country)
@@ -26,51 +29,100 @@ def main():
     # Ask the user weather he would like to continue exploring the dataset by entity
     y_or_n = input("Would you like to investigate by country?(y/n) ")
     if y_or_n == "n":
-        print("\nThank you for using this program. Good bye!\n")
+        pass
     else: 
-        country_of_interest = input("Which country would you like to investigate? ")
-        largest_by_country = find_largest_portion_by_country(dataset, country_of_interest)
+        # Ask the country of interest
+        country_of_interest = input("\nWhich country would you like to investigate? ")
 
-        # Display the years when the country of interest had the largest percent of the population below the line of poverty
-        print(f"The years when {country_of_interest} had the most its people below the poverty line were:")
-        for year in largest_by_country:
+        largest_below = get_largest_country_below(dataset, country_of_interest)
+
+        # Display the years when the country of interest had the largest percent of the population below 
+        # the line of poverty
+        print(f"The years when {country_of_interest} had the most of its people below the poverty line were:")
+        for year in largest_below:
             print(year)
 
-def find_largest_portion_by_year(dataset, year_of_choice, rows):
-    # Find the country with the largest portion of people below the poverty line
+        smallest_below = get_smallest_country_below(dataset, country_of_interest)
+
+        # Display the years when the country of interest had the smallest percent of the population below 
+        # the line of poverty
+        print(f"And the years when it had least people below the poverty line were:")
+        for year in smallest_below:
+            print(year)
+
+    print("\nThank you for using this program. Good bye!\n")
+
+def get_largest_year_below(dataset, year_of_choice, rows):
+    """
+    Get the country with the largest portion of people below the poverty line, tell the user
+    the percentage of people below the poverty line and get the countries with the largest portions 
+    of people below the extreme poverty line.
+    Parameters
+        dataset: The dataset being analysed
+        year_of_coice: The user's choice of year to explore
+        rows: The rows that hold the data for the chosen year
+    Return: the counties with the largest portions of people below the extreme poverty line in the user's chosen 
+    year. 
+    """
     largest_below_poverty = rows.max()
-    # Tell the user the percentage of people below the poverty line
     print(f"\nThe largest number of people below the poverty line in a given country in the year {year_of_choice} is {largest_below_poverty:.2f}%")
     country_with_largest_below_poverty = dataset[(dataset["Year"] == year_of_choice) & (dataset["$30.00 per day - share of population below poverty line"] == largest_below_poverty)]
-
-    # Get the countries with the largest portions of people below the poverty line
     countries_largest_bpl = country_with_largest_below_poverty["Entity"].to_list()
     return countries_largest_bpl
 
-def find_smallest_portion_by_year(dataset, year_of_choice, rows):
-    # Find the country with the smallest portion of people below the poverty line
+def get_smallest_year_below(dataset, year_of_choice, rows):
+    """
+    get the country with the smallest portion of people below the poverty line, tell the user
+    the percentage of people below the poverty line and get the country with the smallest portions 
+    of people below the extreme poverty line.
+    Parameters
+        dataset: The dataset being analysed
+        year_of_coice: The user's choice of year to explore
+        rows: The rows that hold the data for the chosen year
+    Return: A list of counties with the smallest portions of people below the extreme poverty line in the user's 
+    chosen year. 
+    """
     smallest_below_poverty = rows.min()
-
-    # Tell the user the percentage of people below the poverty line
     print(f"And the smallest is {smallest_below_poverty:.2f}%\n")
     country_with_smallest_below_poverty = dataset[(dataset["Year"] == year_of_choice) & (dataset["$30.00 per day - share of population below poverty line"] == smallest_below_poverty)]
-
-    # Get the countries with the smallest portions of people below the poverty line
     countries_smallest_bpl = country_with_smallest_below_poverty["Entity"].to_list()
     return countries_smallest_bpl
 
-def find_largest_portion_by_country(dataset, country_of_interest):
-    # Find the rows of the country of interest
-    coi_rows = dataset.loc[dataset["Entity"] == country_of_interest, "$30.00 per day - share of population below poverty line"]
-
-    # Find the years with the largest portion of people below the poverty line
-    largest_by_country = coi_rows.max()
-    poorest_years = dataset[(dataset["Entity"] == country_of_interest) & (dataset["$30.00 per day - share of population below poverty line"] == largest_by_country)]
+def get_largest_country_below(dataset, country_of_interest):
+    """
+    Get the years when the country of interest had the largest percent of the population below the line of poverty.
+    Parameters
+        dataset: The dataset being analysed.
+        country_of_interest: The user's choice of country to explore.
+    Return: The years where most of the population was under the extreme poverty line.
+    """
+    coi_rows = get_country_of_interest_rows(dataset, country_of_interest)
+    largest_below = coi_rows.max()
+    poorest_years = dataset[(dataset["Entity"] == country_of_interest) & (dataset["$30.00 per day - share of population below poverty line"] == largest_below)]
     poorest_years = poorest_years["Year"].to_list()
     return poorest_years
 
-def find_smallest_portion_by_country():
-    pass
+def get_smallest_country_below(dataset, country_of_interest):
+    """
+    Get the years when the country of interest had the smallest percent of the population below the line of poverty.
+    Parameters
+        dataset: The dataset being analysed.
+        country_of_interest: The user's choice of country to explore.
+    Return: The years where most of the population was above the extreme poverty line.
+    """
+    coi_rows = get_country_of_interest_rows(dataset, country_of_interest)
+    smallest_below = coi_rows.min()
+    richest_years = dataset[(dataset["Entity"] == country_of_interest) & (dataset["$30.00 per day - share of population below poverty line"] == smallest_below)]
+    richest_years = richest_years["Year"].to_list()
+    return richest_years
+
+def get_country_of_interest_rows(dataset, country_of_interest):
+    """
+    get the rows of the country of interest
+    return: the rows with the data of the countries of interest
+    """
+    rows = dataset.loc[dataset["Entity"] == country_of_interest, "$30.00 per day - share of population below poverty line"]
+    return rows
 
 if __name__ == "__main__":
     main()
